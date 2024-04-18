a16d09e7d2963f3bfe5db00ff5b4d3b6ef94b175
Matrix30 <Matrixboy30@gmail.com>
2024-04-19 00:46:55 +0100

    finish task 3. Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+contains function to_str
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    functiont that takes a float and returns
+    the string representation of the float
+    """
+
+    return str(n)
