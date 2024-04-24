8bdecc5ff17b35b7d640c6cdd7a245d386e269d8
Matrix30 <Matrixboy30@gmail.com>
2024-04-24 02:56:40 +0100

    add test file for task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,12 +4,12 @@ contains async_comprehension function
 """
 
 import asyncio
-from typing import Generator
+from typing import List
 
 async_generator = __import__("0-async_generator").async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """
     Import async_generator from the previous task and then write a coroutine
     called async_comprehension that takes no arguments.
