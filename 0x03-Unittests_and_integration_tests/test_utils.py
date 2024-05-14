45443e947252ee8babb8b04df5c9a875552fa407
Matrix30 <Matrixboy30@gmail.com>
2024-05-14 03:38:20 +0100

    to do

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,6 +6,8 @@ contains TestAccessNestedMap class
 from parameterized import parameterized
 from unittest import TestCase
 from utils import access_nested_map
+from unittest.mock import patch
+from utils import memoize
 
 
 class TestAccessNestedMap(TestCase):
@@ -30,3 +32,37 @@ class TestAccessNestedMap(TestCase):
         """method to test keyerror exception in access nested map"""
         with self.assertRaises(KeyError) as e:
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(TestCase):
+    """
+    contains test for utils.getjson methods
+    """
+    def test_get_json(self):
+        """
+        method to use mocking of requests.get to test
+        get_json method
+        """
+        pass
+
+
+class TestMemoize(TestCase):
+    """Tests the `memoize` function."""
+    def test_memoize(self) -> None:
+        """Tests `memoize`'s output."""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as memo_fxn:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memo_fxn.assert_called_once()
