93fdfdb8158089532cf9eb4aca8ba4afa8eb8790
MATRIX30 <matrixboy30@gmail.com>
2025-05-25 21:32:16 +0100

    finish task 2. Concurrent Asynchronous Database Queries

---
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import aiosqlite
+import asyncio
+"""
+Run multiple database queries concurrently
+using asyncio.gather.
+"""
+
+async def async_fetch_users():
+    """
+    fetches all user
+    """
+    async with aiosqlite.connect("users.db") as db:
+        cursor =  await db.execute("SELECT * FROM users")
+        result = await cursor.fetchall()
+    return result
+
+async def async_fetch_older_users():
+    """
+    fetches all users and users older than 40
+    """
+    async with aiosqlite.connect("users.db") as db:
+        cursor =  await db.execute("SELECT * FROM users WHERE age > 40")
+        result = await cursor.fetchall()
+    return result
+
+async def main():
+    result = await asyncio.gather(async_fetch_users(), async_fetch_older_users())
+    print("all Task executed:", result)
+    
+asyncio.run(main())
