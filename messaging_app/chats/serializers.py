4b302ebd2f2ededfeffe08880217ccfe626eaedf
MATRIX30 <matrixboy30@gmail.com>
2025-06-01 13:24:47 +0100

    finish task 2. Create serializers to define the many to many relationships

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -2,13 +2,24 @@ from rest_framework import serializers
 from .models import Conversation, User, Message
 
 class UserSerializer(serializers.ModelSerializer):
+    
+    
+    full_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
     class Meta:
         model = User
         fields = '__all__'
         
         
 class MessageSerializer(serializers.ModelSerializer):
-    sender = serializers.StringRelatedField(read_only = True)
+    # sender = serializers.StringRelatedField(read_only = True)
+    sender = serializers.CharField(source='sender.username', read_only=True)
     class Meta:
         model = Message
         fields = '__all__'
@@ -19,3 +30,12 @@ class ConversationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Conversation
         fields = '__all__'
+    
+    def validate(self, data):
+        """
+        add custom validation logic to your serializer. Specifically, it checks that any conversation being created or updated has at least two participants. If there are fewer 
+        than two, it raises a ValidationError with a helpful message. 
+        """
+        if 'participants' in data and len(data['participants']) < 2:
+            raise serializers.ValidationError("A conversation must have at least two participants.")
+        return data
