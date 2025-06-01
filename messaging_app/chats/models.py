2f5ced01e77a8d4683598a6aa558206f3087bd86
MATRIX30 <matrixboy30@gmail.com>
2025-06-01 11:42:59 +0100

    add settings file

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
 from django.conf import settings
 
 # Create your models here.
@@ -13,8 +14,9 @@ class User(AbstractUser):
         choices=Role.choices,
         default=Role.MEMBER
     )
-    
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     phone_number = models.CharField(max_length=20, blank=False, null=True)
+    email = models.CharField(max_length=55, blank=False, null= True)
     
     def __str__(self):
         return f"{self.id}: {self.first_name} {self.last_name}->{self.role}"
@@ -25,6 +27,7 @@ class Conversation(models.Model):
     """
     #the participant uses a many-to-many relationship with the user model
     #ensuring that any conversation can have multiple users
+    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     participants = models.ManyToManyField(User, related_name='conversations')
     
     created_at = models.DateTimeField(auto_now_add=True)
@@ -40,9 +43,10 @@ class Message(models.Model):
      Each message keeps a record of the sender (linked to the User model) and 
     the conversation it belongs to (linked to the Conversation model).
     """
+    message_id  = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     sender = models.ForeignKey(User,on_delete=models.CASCADE, related_name='messages')
     Conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
-    content = models.TextField()
+    message_body = models.TextField()
     sent_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
