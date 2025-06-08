4ec95a7b8b81ca6849eea0a3838ed77817f6137b
MATRIX30 <matrixboy30@gmail.com>
2025-06-08 20:14:55 +0100

    fix middle ware

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -12,7 +12,7 @@ class RequestLoggingMiddleware:
         # Prepare the log entry with current timestamp, user, and request path.
         log_entry = f"{datetime.now()} - User: {user} - Path: {request.path}\n"
         
-        with open('chats/requests.log', 'a') as log_file:
+        with open('requests.log', 'a') as log_file:
             log_file.write(log_entry)
         
         response = self.get_response(request)
